60da374fa6bc870b3b085f69c604f83f0469e177
Md Shefat Al Mahmud <shefat2002@gmail.com>
2026-01-03 16:42:21 +0600

    client side added

--- GrpcClient/Program.cs
+++ GrpcClient/Program.cs
@@ -1,3 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Grpc.Net.Client;
+using GrpcServicePractice;
 
-Console.WriteLine("Hello, World!");
+using var channel = GrpcChannel.ForAddress("http://localhost:5072");
+var client = new Greeter.GreeterClient(channel);
+var reply = await client.SayHelloAsync(new HelloRequest { Name = "Shefat" });
+
+Console.WriteLine("Greeting: " + reply.Message);
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
